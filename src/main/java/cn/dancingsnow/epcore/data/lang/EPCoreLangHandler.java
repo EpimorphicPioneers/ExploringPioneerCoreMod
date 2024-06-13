a9b53fef0ad0871674218729dbe1d074f39d86f2
DancingSnow <1121149616@qq.com>
2024-06-13 13:33:56 +0800

    材料，tagprefix语言文件

--- src/main/java/cn/dancingsnow/epcore/data/lang/EPCoreLangHandler.java
+++ src/main/java/cn/dancingsnow/epcore/data/lang/EPCoreLangHandler.java
@@ -1,5 +1,6 @@
 package cn.dancingsnow.epcore.data.lang;
 
+import cn.dancingsnow.epcore.common.data.EPCoreTagPrefixes;
 import cn.dancingsnow.epcore.common.data.worldgen.EPCoreOreVeins;
 
 import com.epimorphismmc.monomorphism.datagen.lang.MOLangProvider;
@@ -10,9 +11,13 @@ public class EPCoreLangHandler {
 
     public static void init(MOLangProvider provider) {
         EPCoreBlockLang.init(provider);
+        EPCoreMaterialLang.init(provider);
         EPCoreWorldLang.init(provider);
 
         EPCoreOreVeins.ALL_ORES.forEach(
                 ore -> provider.add(ore.getTranslateKey(), ore.en_name(), ore.cn_name()));
+
+        EPCoreTagPrefixes.ALL_TAG_PREFIXES.forEach(
+                prefix -> provider.add(prefix.getTranslateKey(), prefix.en_lang(), prefix.cn_lang()));
     }
 }
